62b41ba8ae7b6861586cfa171d28146fd07ad1d3
himaloyash <himaloyashis98@gmail.com>
2023-11-02 22:38:45 +0600

    mongodb

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-require('dotenv').config
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 const cors = require('cors')
+require('dotenv').config()
 
 
 app.use(cors())
 app.use(express.json())
 
+// Devtown-prod
+// HpyJ0G2RjpKMm7Df
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.6ogtg9l.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.6ogtg9l.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,13 +27,24 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect()
+    const userDBCollection = client.db('DevProd').collection('videosProd')
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+
+
+
+app.get('/', (req, res)=>{
+  res.send('devTown is on live now')
+})
+
+app.listen(port, ()=>{
+  console.log(`DevTown is running port : ${port}`)
+})
